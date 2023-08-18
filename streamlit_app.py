26f32fd361cc8e0c73fd2f355d94c420335de470
Todd Rollins <63811857+tprollins@users.noreply.github.com>
2023-08-18 14:47:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.text (Omega 3 & Blueberry Oatmeal)
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
 
-streamlit.text (Kale, Spinach & Rocket Smoothie)
+streamlit.text ('Kale, Spinach & Rocket Smoothie')
 
-streamlit.text (Hard-Boiled Free-Range Egg)
+streamlit.text ('Hard-Boiled Free-Range Egg')
