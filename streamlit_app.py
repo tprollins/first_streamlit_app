f9c1a9dd4e2d89a1aab692185f3bcc59eba75383
Todd Rollins <63811857+tprollins@users.noreply.github.com>
2023-08-30 09:53:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-steamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
 
