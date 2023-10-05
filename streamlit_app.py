0e980ea0b5bae4099ec4f9513811bb464ee54e8a
Todd Rollins <63811857+tprollins@users.noreply.github.com>
2023-10-05 08:58:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+#cleaning up the data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#brings in the clean data
+streamlit.dataframe(fruityvice_normalized)
